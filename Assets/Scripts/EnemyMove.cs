a5378980bd2d878f9d4b2c4bdc2dadee79d6ddf1
Djioop <djioup@protonmail.com>
2021-10-12 22:25:05 +0200

    ennemi qui bouge

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject point1, point2, point3, point4;
+    [SerializeField] private float speed;
+    private float vpointX, vpointY;
+
+    private void Start()
     {
-        
+        vpointX = Random.Range(point1.transform.position.x, point2.transform.position.x);
+        vpointY = Random.Range(point3.transform.position.y, point4.transform.position.y);
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(vpointX, vpointY, 0), speed * Time.deltaTime);
+
+        if (transform.position == new Vector3 (vpointX, vpointY, 0))
+        {
+            RandomPoint();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RandomPoint ()
     {
-        
+        vpointX = Random.Range(point1.transform.position.x, point2.transform.position.x);
+        vpointY = Random.Range(point3.transform.position.y, point4.transform.position.y);
     }
 }
