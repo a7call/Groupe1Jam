e425a77a6f7fb50837f4ff20ec1844584197437a
Pbry <38502102+Pbry@users.noreply.github.com>
2021-10-12 22:48:10 +0200

    Enabling changing materials in FlyCollider

--- Assets/Scripts/FlyCollider.cs
+++ Assets/Scripts/FlyCollider.cs
@@ -9,13 +9,13 @@ public class FlyCollider : MonoBehaviour
     [SerializeField] private bool CanDie;
     [SerializeField] private Material blue, orange;
     public bool enable;
-    Material material;
+    MeshRenderer meshRenderer;
     Collider collide;
     Animator animat;
 
     void Start ()
     {
-        material = GetComponent<Renderer>().material;
+        meshRenderer = GetComponent<MeshRenderer>();
 
         if (col)
         {
@@ -37,8 +37,8 @@ public class FlyCollider : MonoBehaviour
                 collide.enabled = false;
                 enable = false;
                 print("Collider OFF");
-                material.color = Color.red;
-
+                meshRenderer.material = orange;
+                
                 if (CanDie)
                 {
                     Destroy(gameObject);
@@ -49,7 +49,7 @@ public class FlyCollider : MonoBehaviour
             {
                 animat.enabled = false;
                 enable = false;
-                material.color = Color.red;
+                meshRenderer.material = orange;
 
                 if (CanDie)
                 {
@@ -65,7 +65,7 @@ public class FlyCollider : MonoBehaviour
                 collide.enabled = true;
                 enable = true;
                 print("Collider ON");
-                material.color = Color.blue;
+                meshRenderer.material = blue;
 
                 if (CanDie)
                 {
@@ -77,7 +77,7 @@ public class FlyCollider : MonoBehaviour
             {
                 animat.enabled = true;
                 enable = true;
-                material.color = Color.blue;
+                meshRenderer.material = blue;
 
                 if (CanDie)
                 {
