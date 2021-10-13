cc5efe4ad9dcda26faf67d324311db68db836870
Djioop <djioup@protonmail.com>
2021-10-13 12:43:05 +0200

    Salle 2 et 3

---
+++ Assets/ExitCount.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitCount : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> enemy = new List<GameObject>();
+    [SerializeField] private GameObject Exit;
+
+    private void Update()
+    {
+        foreach (var EN in enemy.ToArray())
+        {
+            if (EN == null)
+            {
+                enemy.Remove(EN);
+            }
+        }
+        if (enemy.Count == 0)
+        {
+            Exit.SetActive(true);
+        }
+    }
+}
