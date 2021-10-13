b858fded22af056e7d4a2cdad0500ae2f05a1609
Djioop <djioup@protonmail.com>
2021-10-13 12:54:35 +0200

    Fix

--- Assets/Exit.cs
+++ Assets/Exit.cs
@@ -6,9 +6,10 @@ using UnityEngine.SceneManagement;
 public class Exit : MonoBehaviour
 {
     [SerializeField] private int NumScene;
+    [SerializeField] private GameObject Player1, Player2;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag ("Player"))
+        if (Player1 && Player2)
         {
             SceneManager.LoadScene(NumScene);
         }
